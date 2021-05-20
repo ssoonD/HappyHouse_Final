e720f49cfe298f1657f155b98279b9db2e24f193
Hyun-corleone <pinkfloyd951@naver.com>
2021-05-21 01:06:40 +0900

    event

--- HappyHouse_FrontEnd_Vuejs/src/App.vue
+++ HappyHouse_FrontEnd_Vuejs/src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="boxwrap">
+    <div class="box" style="background-color:red;">1</div>
+    <div class="box" style="background-color:orange;">2</div>
+    <div class="box" style="background-color:yellow;">3</div>
+    <div class="box" style="background-color:green;">4</div>
+    <div class="box" style="background-color:blue;">5</div>
+    <div class="box" style="background-color:indigo;">6</div>
+    <div class="box" style="background-color:violet;">7</div>
     </div>
-    <router-view />
-  </div>
 </template>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,4 +32,12 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
+
+<style type="text/css">
+        html,body{ margin:0; padding:0; width:100%; height:100%;}
+        .boxwrap{ display: table; table-layout: fixed; width: 700%; height: 100%; table-layout: fixed;}
+        .box{ display: table-cell;position:relative; color:#ffffff; font-size:24pt;}
+</style>
+
